5a33aa9ce258c1f34606e4df2539fea2d5986cfc
lucasjpl1 <mitilenda123@gmail.com>
2025-12-01 14:08:55 -0300

    questão8

---
+++ Questão8.js
@@ -0,0 +1,13 @@
+function somaImpares(n) {
+    let soma = 0;
+    for (let i = 1; i <= n; i++ ) {
+        if (i % 2 !== 0) {
+            soma += i;
+        }
+    }
+    return soma;
+}
+
+
+console.log(somaImpares(5)); 
+console.log(somaImpares(10));
